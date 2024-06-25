238e0d46d3e67996f57458a0fd6db2530774e02e
Sandip <roysandip33290@gmail.com>
2024-06-25 11:49:22 +0530

    improved nav bar and added auth functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 const Homepage = () => {
   return (
-    <div className='font-bold'>Homepage</div>
+    <div className='flex gap-6'>
+      <div className="hidden xl:block w-[20%]">right</div>
+      <div className="w-full lg:w-[70%] xl:w-[50%]">center</div>
+      <div className="hidden lg:block w-[30%]">left</div>
+    </div>
   )
 }
 
